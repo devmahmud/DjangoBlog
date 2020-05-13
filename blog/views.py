ea3d459f6c2618125a8187a729d76e9f3bfcfd0b
Mahmudul Alam <expelmahmud@gmail.com>
2020-05-13 16:41:43 +0600

    Add tag and pagination

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, TemplateView
+from taggit.models import Tag
+
+from .models import Post
 
 
-class PostListView(TemplateView):
+class PostListView(ListView):
+    paginate_by = 3
     template_name = 'blog/post-list.html'
+
+    def get_queryset(self):
+        queryset =  Post.published.all()
+        tag_slug = self.kwargs.get('tag_slug')
+        if tag_slug:
+            tag = get_object_or_404(Tag, slug=tag_slug)
+            queryset = queryset.filter(tags__in=[tag])
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tag"] = self.kwargs.get('tag_slug')
+        return context
+
 
 class PostDetailView(TemplateView):
     template_name = 'blog/post-detail.html'
