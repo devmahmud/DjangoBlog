a42dc9b4cfc23a89c77cdabb63524ace245c840d
Mahmudul Alam <expelmahmud@gmail.com>
2020-05-13 17:37:36 +0600

    Add Comment Model

--- blog/models.py
+++ blog/models.py
@@ -51,3 +51,19 @@ class Post(models.Model):
                 self.slug
             ]
         )
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+    
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
