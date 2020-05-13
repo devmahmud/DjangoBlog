16b73c75bc58d3ef92939c95ebeea2a04bb4b55f
Mahmudul Alam <expelmahmud@gmail.com>
2020-05-13 15:27:50 +0600

    Add Media URL

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', include('blog.urls', namespace='blog')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
